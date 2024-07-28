e688d84e71913de8e33c2b2aad13894173d20b81
Arthur Ryzhak <ryzhakar@gmail.com>
2024-07-28 22:12:10 +0300

    feat(snic): network matchups iteration

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,13 @@ mod common_types;
 mod common_utilities;
 
 fn main() {
-    let original_number = common_types::InputInt::MAX;
-    let base = common_types::BaseInt::MAX - 1;
-    let r = dbg!(gber::GBERepresentation::new(original_number, base));
-    assert_eq!(r.to_decimal(), original_number);
-    let mut running_matchups_sum = 0u64;
-    let mut running_comparisons_sum = 0u64;
-    for subnetwork_size in dbg!(r.calculate_components()) {
-        let matchups = dbg!(network::calculate_matchups_number_for(subnetwork_size, r.base));
-        let comparisons = dbg!(network::calculate_comparisons_number_for(matchups, r.base));
-        running_matchups_sum += matchups;
-        running_comparisons_sum += comparisons;
-    }
-    if dbg!(r.remainder > 0) {
-        running_matchups_sum += 1;
-        running_comparisons_sum += dbg!(network::calculate_comparisons_number_for(1, r.base));
-    }
-    dbg!(running_matchups_sum, running_comparisons_sum);
+    // let original_number = common_types::InputInt::MAX - 1;
+    let original_number = 2u32.pow(10) - 1;
+    let max_base = 19;
+    let ntwk = network::StreamNetworkMatchups::new(
+        gber::GBERepresentation::new(original_number, max_base)
+    );
+    for matchup in ntwk {
+        dbg!(matchup);
+    };
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,5 +1,7 @@
+use crate::gber;
 use crate::common_types::{InputInt, BaseInt};
 use crate::common_utilities;
+use std::collections::VecDeque;
 
 
 pub fn calculate_matchups_number_for(
@@ -19,4 +21,138 @@ pub fn calculate_comparisons_number_for(
     let base: InputInt = base.into();
     let per_matchup: InputInt = base * (base-1) / 2;
     matchups_number * per_matchup as u64
+}
+
+
+#[derive(Default, Debug)]
+pub struct MatchupIterator {
+    pub network_size: InputInt,
+    pub matchup_size: BaseInt,
+    pub offset: InputInt,
+    level: u8,
+    // Current item, exclusive end item
+    section_states: Vec<(InputInt, InputInt)>,
+    section_group_head: usize,
+}
+
+impl MatchupIterator {
+
+    pub fn new(
+        network_size: InputInt,
+        matchup_size: BaseInt,
+        offset: InputInt,
+    ) -> Result<Self, String> {
+        Ok(Self {
+            network_size,
+            matchup_size,
+            offset,
+            ..Default::default()
+        })
+    }
+    fn init_level(&mut self) -> Option<()> {
+        self.level += 1;
+        let sections_number = self.calculate_sections_number();
+        if sections_number > self.network_size {
+            return None;
+        }
+        self.section_states.clear();
+        let section_size: InputInt = self.network_size / sections_number;
+        let mut head: InputInt = 0;
+        let rangetails = (section_size..self.network_size + 1).filter(
+            |&ix| ix % section_size == 0
+        );
+        self.section_states = rangetails.map(
+            |tail| {
+                let section = (head, tail);
+                head += section_size;
+                section
+            }
+        ).collect();
+        self.section_group_head = 0;
+        Some(())
+    }
+
+    fn calculate_sections_number(&self) -> InputInt {
+        (self.matchup_size as InputInt).pow(self.level as InputInt)
+    }
+    fn increment_internal_section_pointer(&mut self, index: usize) {
+        self.section_states[index].0 += 1;
+    }
+    fn increment_group_head_if_exhausted(&mut self) {
+        let (ctrlhead, ctrltail) = self.section_states[self.section_group_head];
+        if ctrltail == ctrlhead {
+            self.section_group_head += self.matchup_size as usize;
+        };
+    }
+}
+
+impl Iterator for MatchupIterator {
+    type Item = Vec<InputInt>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.section_group_head >= self.section_states.len() {
+            self.init_level()?
+        };
+        let group_size = self.matchup_size as usize;
+        let sections = self.section_states.clone();
+
+        let selected_sections = sections.iter().enumerate()
+            .skip(self.section_group_head)
+            .take(group_size)
+        ;
+        let matchup: Vec<InputInt> = selected_sections
+            .map(|(ix, section)| {
+                let old_head = section.0.clone();
+                self.increment_internal_section_pointer(ix);
+                old_head + self.offset
+            })
+            .collect();
+        self.increment_group_head_if_exhausted();
+
+        Some(matchup)
+    }
+
+}
+
+#[derive(Default, Debug)]
+pub struct StreamNetworkMatchups {
+    pub network_gber: gber::GBERepresentation,
+    term_iterators: VecDeque<MatchupIterator>,
+}
+
+impl StreamNetworkMatchups {
+    pub fn new(network_gber: gber::GBERepresentation) -> Self {
+        let mut rolling_offset: InputInt = 0;
+        let term_iterators = network_gber.calculate_components().iter()
+            .map(
+                |sn_size| {
+                    let iter = MatchupIterator::new(sn_size.clone(), network_gber.base, rolling_offset).unwrap();
+                    rolling_offset += sn_size;
+                    iter
+                }
+        ).collect();
+        Self {
+            network_gber,
+            term_iterators,
+        }
+    }
+}
+
+impl Iterator for StreamNetworkMatchups {
+    type Item = Vec<InputInt>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut titlen = self.term_iterators.len();
+        while titlen > 0 {
+            let current_stream = self.term_iterators.iter_mut().next()?;
+            if let Some(current_match) = current_stream.next() {
+                return Some(current_match)
+            } else {
+                let _ = &mut self.term_iterators.pop_front();
+                titlen = self.term_iterators.len();
+                continue
+            };
+        }
+        None
+    }
 }
