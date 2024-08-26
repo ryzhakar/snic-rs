f40211dc957bba9667aa18eb3a5aded00481e274
Arthur Ryzhak <ryzhakar@gmail.com>
2024-08-26 21:00:38 +0300

    feat(comparison): retrieve matchup result elements in pairs

--- snic_core/src/network/comparison.rs
+++ snic_core/src/network/comparison.rs
@@ -1,7 +1,7 @@
 use crate::common_types::BaseInt;
 
 /// Generate pairwise combinations of indices for given size
-fn generate_expansion_mould_for(size: BaseInt) -> Vec<(BaseInt, BaseInt)> {
+pub fn generate_expansion_mould_for(size: BaseInt) -> Vec<(BaseInt, BaseInt)> {
     let mut comparison_indices: Vec<(BaseInt, BaseInt)> = vec!();
     for left_pointer in 0..(size-1) {
         for right_pointer in (left_pointer+1)..size {
@@ -12,8 +12,39 @@ fn generate_expansion_mould_for(size: BaseInt) -> Vec<(BaseInt, BaseInt)> {
 }
 
 
+/// Retrieve elements by pairwaise indices mould
+pub fn convert_to_comparisons<T: Clone>(
+    matchup_result: &[T],
+    mould: &[(BaseInt, BaseInt)],
+) -> Vec<(T, T)> {
+    mould.iter().map(|(lix, rix)| {
+        (
+            matchup_result[lix.clone() as usize].clone(),
+            matchup_result[rix.clone() as usize].clone()
+        )
+    }).collect()
+}
+
+
 #[cfg(test)]
-mod test_comparison_expansion {
+mod test_matchup_result_conversion {
+    use super::convert_to_comparisons;
+    use crate::common_types::BaseInt;
+    const MATCHUP_SIZE: BaseInt = u8::MAX as BaseInt;
+
+    #[test]
+    fn test_elements_retrieval() {
+        let indices = (0, MATCHUP_SIZE - 1);
+        let values = indices.clone();
+        let mould: &[(BaseInt, BaseInt)] = &[indices];
+        let matchup_results = &(0..MATCHUP_SIZE).collect::<Vec<BaseInt>>()[..];
+        assert_eq!(convert_to_comparisons(matchup_results, mould), vec![values]);
+    }
+}
+
+
+#[cfg(test)]
+mod test_mould_generation {
     use super::generate_expansion_mould_for;
     use crate::common_types::{BaseInt, InputInt};
     const MATCHUP_SIZE: BaseInt = u8::MAX as BaseInt;
