dc28f31a3576704af71345d6d7dd8e7809a1cca2
Arthur Ryzhak <ryzhakar@gmail.com>
2024-09-11 12:29:10 +0300

    feat(python): expose ranking to python lib

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,37 @@ fn stream_matches_from<'py>(
         ).collect()
 }
 
+
+#[pyfunction]
+#[pyo3(signature = (ranked_matches))]
+fn stream_rankings_from<'py>(
+    _py: Python<'py>,
+    ranked_matches: Vec<Vec<common_types::InputInt>>,
+) -> Vec<common_types::InputInt> {
+    let expansion_mould =
+        network::comparison::generate_expansion_mould_for(
+            ranked_matches[0].len() as common_types::BaseInt
+    );
+    let pairwise_comparisons = ranked_matches.into_iter()
+        .flat_map(
+            |ranking| {
+                network::comparison::convert_to_comparisons(
+                    &ranking[..],
+                    &expansion_mould[..],
+                )
+            }
+        ).collect::<Vec<(common_types::InputInt, common_types::InputInt)>>();
+    let rank_scores = network::rank::get_ranking_from(pairwise_comparisons);
+    let mut indices = (0..(rank_scores.len() as common_types::InputInt))
+        .collect::<Vec<common_types::InputInt>>();
+    indices.sort_by(|a, b| rank_scores[*a as usize].partial_cmp(&rank_scores[*b as usize]).unwrap());
+    indices
+}
+
 #[pymodule]
 fn snic(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(stream_matches_from, m)?)?;
+    m.add_function(wrap_pyfunction!(stream_rankings_from, m)?)?;
     Ok(())
 }
 
